f35f65698a8ac2f538861970235d68b47119760a
Juuso Puroila <jpuroila@gmail.com>
2017-11-21 14:34:47 +0200

    Update apache configuration

--- apache/manifests/init.pp
+++ apache/manifests/init.pp
@@ -4,9 +4,26 @@ class apache{
 		ensure=>'latest',
 		provider=>'chocolatey',
 	}
+	file{'C:\Users\Projekti\AppData\Roaming\Apache24\conf\httpd.conf':
+		ensure=>file,
+		content=>template('apache/httpd.conf.erb'),
+	}
+	file{'C:\Users\Projekti\AppData\Roaming\Apache24\conf\extra\httpd-vhosts.conf':
+                ensure=>file,
+                content=>template('apache/httpd-vhosts.conf.erb'),
+        }
+	file{'C:\Users\Projekti\www\index.html':
+                ensure=>file,
+                content=>template('apache/index.html.erb'),
+        }
+	file{'C:\Users\Projekti\AppData\Roaming\Apache24\htdocs\first.html':
+                ensure=>file,
+                content=>template('apache/first.html.erb'),
+        }
+
 }
 
-httpd.conf C:\Users\Projekti\AppData\Roaming\Apache24\conf
-httpd-vhost.conf C:\Users\Projekti\AppData\Roaming\Apache24\conf\extra
-index.html  C:\Users\Projekti\www
-first.html C:\Users\Projekti\AppData\Roaming\Apache24\htdocs
+# httpd.conf C:\Users\Projekti\AppData\Roaming\Apache24\conf
+# httpd-vhost.conf C:\Users\Projekti\AppData\Roaming\Apache24\conf\extra
+# index.html  C:\Users\Projekti\www
+# first.html C:\Users\Projekti\AppData\Roaming\Apache24\htdocs
