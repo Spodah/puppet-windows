36ef6329b9c4de32f9453cc3c2fbdb6e22728d98
Niklas Graniittiaho <niklas.graniittiaho@myy.haaga-helia.fi>
2017-11-21 13:11:10 +0200

    added apache config

--- apache/manifests/init.pp
+++ apache/manifests/init.pp
@@ -5,3 +5,8 @@ class apache{
 		provider=>'chocolatey',
 	}
 }
+
+httpd.conf C:\Users\Projekti\AppData\Roaming\Apache24\conf
+httpd-vhost.conf C:\Users\Projekti\AppData\Roaming\Apache24\conf\extra
+index.html  C:\Users\Projekti\www
+first.html C:\Users\Projekti\AppData\Roaming\Apache24\htdocs
